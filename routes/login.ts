75b19025e9d13e263ef7e924791cfc96e3106472
s.gioria <s.gioria@lectra.com>
2023-11-11 11:45:17 +0100

    chore: add comment for basic injection

--- routes/login.ts
+++ routes/login.ts
@@ -33,6 +33,9 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
+    // CH-01-Injection
+    // Here the injection of login
+    // Simple SQL Injection
     models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
